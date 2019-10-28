41acacd631c3039af157f8326638075d6261a8de
MelanieJaneBond <miss.bond2@gmail.com>
2019-10-28 10:17:47 -0500

    had to add an initial default value to boolean field

--- jobsauceapp/models/response.py
+++ jobsauceapp/models/response.py
@@ -7,8 +7,8 @@ class Response(models.Model):
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     job = models.ForeignKey(Job, on_delete=models.CASCADE)
-    details = models.CharField(max_length=400)
-    is_rejected = models.BooleanField()
+    details = models.CharField(max_length=400, null=True, blank=True)
+    is_rejected = models.BooleanField(initial=False)
     date = models.DateTimeField(auto_now_add=True, blank=True)
 
     class Meta:
