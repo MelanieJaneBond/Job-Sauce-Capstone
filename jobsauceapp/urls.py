4398775d0f0c4738b412bae8070f98faaf64a7f7
MelanieJaneBond <miss.bond2@gmail.com>
2019-10-28 15:47:48 -0500

    worked on urls.py in both project and app

--- jobsauceapp/urls.py
+++ jobsauceapp/urls.py
@@ -1,18 +1,21 @@
 from django.conf.urls import url
 from django.conf.urls import url, include
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from .views import *
 
 app_name = "jobsauceapp"
 
 urlpatterns = [
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('logout/', logout_user, name='logout'),
-    path('jobs', job_list, name='job_list'),
-    path('job/form', job_form, name='job_form'),
-    path('responses', response_list, name='response_list'),
-    path('response/form', response_form, name='response_form'),
-    path('connections', social_connection_list, name='social_connection_list'),
-    path('resources', study_resource_list, name='study_resource_list'),
-    path('resource/form', study_resource_form, name='study_resource_form')
+    path('jobs', job_list, name='list'),
+    # path('job/form', job_form, name='job_form'),
+    # path('responses', response_list, name='response_list'),
+    # path('response/form', response_form, name='response_form'),
+    # path('connections', social_connection_list, name='social_connection_list'),
+    # path('resources', study_resource_list, name='study_resource_list'),
+    # path('resource/form', study_resource_form, name='study_resource_form'),
+
+    url(r'accounts/', include('django.contrib.auth.urls')),
+    # url(r'^logout/$', logout_user, name='logout'),
+    # url(r'^register/$', register_user, name='register'),
 ]
