51724d5895f4547112aaf0be73d40c62a8d02e2f
MelanieJaneBond <miss.bond2@gmail.com>
2019-11-05 11:17:44 -0600

    not done yet

--- jobsauceapp/views/job/detail.py
+++ jobsauceapp/views/job/detail.py
@@ -2,52 +2,109 @@ import sqlite3
 from django.urls import reverse
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from jobsauceapp.models import Job, Response, Company
+from jobsauceapp.models import Job, Company, Job_Tech
 from ..connection import Connection
 
+#get one of job, company, and job_techs
+#you will be PUTTING for EACH ID so, you need a details.py
+# to organize the functionality of your code
+def create_company(cursor, row):
+    row = sqlite3.Row(cursor, row)
 
+    company = Company()
+    company.id = row[0]
+    company.name = row[1]
 
-def get_job(job_id):
+    return (company,)
+
+def create_job(cursor, row):
+    row = sqlite3.Row(cursor, row)
+
+    job = Job()
+    job.id = row[0]
+    job.title_of_position = row[1]
+    job.date_of_submission = row[2]
+    job.company_id = row[3]
+    job.tech_list_id = row[4]
+    job.user_id = row[5]
+
+    return (job,)
+
+def create_job_tech(cursor, row):
+
+    job_tech = Job_Tech()
+    job_tech.id = row[0]
+    job_tech.tech_type_id = row[1]
+    job_tech.job_id = row[2]
+
+    return (job_tech,)
+
+def get_company(company_id):
     with sqlite3.connect(Connection.db_path) as conn:
-        conn.row_factory = model_factory(Library)
+        conn.row_factory = create_company
         db_cursor = conn.cursor()
 
         db_cursor.execute("""
             SELECT
-                l.id,
-                l.title,
-                l.address
-            FROM libraryapp_library l
-            WHERE l.id = ?
-            """, (library_id,)
+                c.id,
+                c.name
+            FROM jobsauceapp_company c
+            WHERE c.id = ?
+            """, (company_id,)
         )
 
         return db_cursor.fetchone()
 
-def get_libraries():
+def get_job(job_id):
     with sqlite3.connect(Connection.db_path) as conn:
-        conn.row_factory = model_factory(Library)
+        conn.row_factory = create_job
         db_cursor = conn.cursor()
 
         db_cursor.execute("""
-        select
-            l.id,
-            l.title,
-            l.address
-        from libraryapp_library l
-        """)
+            SELECT
+                j.id,
+                j.title_of_position,
+                j.date_of_submission,
+                j.company_id,
+                j.tech_list_id,
+                j.tech_list_id,
+                j.user_id
+            FROM jobsauceapp_job j
+            WHERE j.id = ?
+            """, (job_id,)
+        )
 
-        return db_cursor.fetchall()
+        return db_cursor.fetchone()
 
+def get_job_tech(job_id):
+    with sqlite3.connect(Connection.db_path) as conn:
+        conn.row_factory = create_job_tech
+        db_cursor = conn.cursor()
 
-@login_required
-def library_details(request, library_id):
+        db_cursor.execute("""
+            SELECT
+                jt.id,
+                jt.tech_type_id,
+                jt.job_id
+            FROM jobsauceapp_job_tech jt
+            WHERE jt.job_id = ?
+            """, (job_id,)
+        )
+
+        return db_cursor.fetchone()
+
+# @login_required
+def job_details(request, company_id, job_id):
     if request.method == 'GET':
 
-        library = get_library(library_id)
-        template = 'libraries/detail.html'
+        company = get_company(company_id)
+        job = get_job(job_id)
+        job_tech = get_job_tech(job_id)
+        template = 'jobs/detail.html'
         context = {
-            'library': library
+            'company': company,
+            'job': job,
+            'job_tech': job_tech
         }
 
         return render(request, template, context)
@@ -55,7 +112,7 @@ def library_details(request, library_id):
     elif request.method == 'POST':
         form_data = request.POST
 
-        # Check if this POST is for editing a library
+        # Check if this POST is for editing instead of creating
         if (
             "actual_method" in form_data
             and form_data["actual_method"] == "PUT"
@@ -64,21 +121,47 @@ def library_details(request, library_id):
                 db_cursor = conn.cursor()
 
                 db_cursor.execute("""
-                    UPDATE libraryapp_library
-                    SET title = ?,
-                        address = ?
-                    WHERE id = ?
-                    """,
-                    (
-                        form_data['title'],
-                        form_data['address'],
-                        library_id,
-                    )
-                )
+                UPDATE jobsauceapp_company
+                SET name = ?,
+                WHERE id = ?
+                """,
+                (form_data['company_name'], company_id,))
 
-            return redirect(reverse('libraryapp:libraries'))
+            with sqlite3.connect(Connection.db_path) as conn:
+                db_cursor = conn.cursor()
 
-        # Check if this POST is for deleting a library
+                db_cursor.execute("""
+                UPDATE jobsauceapp_job
+                SET title_of_position = ?,
+                    date_of_submission = ?,
+                    company_id = ?,
+                    tech_list_id = ?,
+                    user_id = ?,
+                WHERE id = ?
+                """,
+                (form_data['title_of_position'], form_data['date_of_submission'],
+                    company_id, None, request.user.id))
+    
+    #delete all the techs they created before, when they choose to edit the techs, all will go away and
+    # a new list of techs will be created when they hit submit on the edit form.
+    #I will need to fetch the ID of the job they are editing... or the ID of the job they just edited ?
+    # using that id, I will write a SQL delete and then an INSERT INTO for the job_tech table
+
+            with sqlite3.connect(Connection.db_path) as conn:
+                db_cursor = conn.cursor()
+                nothing = None
+
+                db_cursor.execute("""
+                UPDATE jobsauceapp_job_tech
+                SET tech_type_id = ?
+                WHERE job_id = ?
+                """,
+                (form_data['technologies_list'], job_id,))
+            
+            
+            return redirect(reverse('jobsauceapp:jobs'))
+
+        # Check if this POST is for deleting instead of creating
         if (
             "actual_method" in form_data
             and form_data["actual_method"] == "DELETE"
